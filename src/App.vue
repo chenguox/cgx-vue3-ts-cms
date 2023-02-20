164aa3ede01e90bc5712fd6e9cc707ea1010299c
codercgx <529488618@qq.com>
2023-02-20 19:49:47 +0800

    feat: 集成Element-Plus组件库

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@
   <router-link to="/main">主要</router-link>
   <router-link to="/login">登录</router-link>
   <router-view></router-view>
+  <el-row class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
 </template>
 
 <script setup lang="ts"></script>
